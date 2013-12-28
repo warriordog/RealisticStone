f27262e4452884acd33a1dbfddc3379cdff3e821
warriordog <acomputerdog@gmail.com>
2013-12-28 09:19:00 -0500

    Add code

---
+++ src/net/acomputerdog/RealisticStone/BlockRealisticStone.java
@@ -0,0 +1,43 @@
+package net.acomputerdog.RealisticStone;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockStone;
+import net.minecraft.block.MinecraftPackageAccessorC;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.world.World;
+
+import java.util.Random;
+
+public class BlockRealisticStone extends BlockStone {
+
+    protected BlockRealisticStone(Block oldStone) {
+        super();
+        this.func_149711_c(1.5F);
+        this.func_149752_b(10.0F);
+        this.func_149672_a(field_149780_i);
+        this.func_149663_c("stone");
+        this.func_149658_d("stone");
+        this.field_149761_L = MinecraftPackageAccessorC.getBlockIcon(oldStone);
+    }
+
+    public Item func_149650_a(int p_149650_1_, Random p_149650_2_, int p_149650_3_)
+    {
+        return Item.func_150898_a(Blocks.field_150347_e);
+    }
+
+    public float func_149712_f(World world, int x, int y, int z)
+    {
+        if (y <= 11) {
+            return field_149782_v * 2;
+        } else if (y <= 32) {
+            return (float)(field_149782_v * 1.75);
+        } else if (y <= 48) {
+            return (float)(field_149782_v * 1.5);
+        } else if (y <= 64) {
+            return (float)(field_149782_v * 1.25);
+        } else {
+            return field_149782_v;
+        }
+    }
+}
---
+++ src/net/acomputerdog/RealisticStone/ModRealisticStone.java
@@ -0,0 +1,110 @@
+package net.acomputerdog.RealisticStone;
+
+import net.acomputerdog.BlazeLoader.api.block.ApiBlock;
+import net.acomputerdog.BlazeLoader.main.Version;
+import net.acomputerdog.BlazeLoader.mod.Mod;
+import net.acomputerdog.BlazeLoader.util.logger.BLLogger;
+import net.minecraft.block.Block;
+
+public class ModRealisticStone extends Mod {
+    public static final BLLogger logger = new BLLogger("Realistic_Stone", false, false);
+
+    /**
+     * Returns ID used to identify this mod internally, even among different versions of the same mod.  Mods should override.
+     * --This should never be changed after the mod has been released!--
+     *
+     * @return Returns the id of the mod.
+     */
+    @Override
+    public String getModId() {
+        return "acomputerdog.realisticstone";
+    }
+
+    /**
+     * Returns the user-friendly name of the mod.  Mods should override.
+     * --Can be changed among versions, so this should not be used to ID mods!--
+     *
+     * @return Returns user-friendly name of the mod.
+     */
+    @Override
+    public String getModName() {
+        return "Realistic Stone";
+    }
+
+    /**
+     * Gets the version of the mod as an integer for choosing the newer version.
+     *
+     * @return Return the version of the mod as an integer.
+     */
+    @Override
+    public int getIntModVersion() {
+        return 0;
+    }
+
+    /**
+     * Gets the version of the mod as a String for display.
+     *
+     * @return Returns the version of the mod as an integer.
+     */
+    @Override
+    public String getStringModVersion() {
+        return "0.0";
+    }
+
+    /**
+     * Returns true if this mod is compatible with the installed version of BlazeLoader.  This should be checked using Version.class.
+     * -Called before mod is loaded!  Do not depend on Mod.load()!-
+     *
+     * @return Returns true if the mod is compatible with the installed version of BlazeLoader.
+     */
+    @Override
+    public boolean isCompatibleWithBLVersion() {
+        if(!Version.getMinecraftVersion().equals("1.7.2")){
+            System.out.println("TerrainEdit - Incorrect Minecraft version, aborting launch!");
+            return false;
+        }else if(Version.getGlobalVersion() == 2 && Version.getApiVersion() > 3){
+            System.out.println("TerrainEdit - Unknown BL version, bad things may happen!");
+            return true;
+        }else if(Version.getGlobalVersion() != 2 || Version.getApiVersion() <= -1){
+            System.out.println("TerrainEdit - Incompatible BL version, aborting launch!");
+            return false;
+        }else{
+            System.out.println("TerrainEdit - Valid BL and MC versions.");
+            return true;
+        }
+    }
+
+    /**
+     * Gets a user-friendly description of the mod.
+     *
+     * @return Return a String representing a user-friendly description of the mod.
+     */
+    @Override
+    public String getModDescription() {
+        return "Makes stone harder at deeper depths";
+    }
+
+    /**
+     * Called when mod is started.  Game is fully loaded and can be interacted with.
+     */
+    @Override
+    public void start() {
+        ApiBlock.registerBlock(new BlockRealisticStone((Block)Block.field_149771_c.getObject("stone")), "stone", 1);
+    }
+
+    /**
+     * Called when mod is loaded.  Called before game is loaded.
+     */
+    @Override
+    public void load() {
+        logger.logInfo("Realistic Stone is starting.");
+    }
+
+    /**
+     * Called when mod is stopped.  Game is about to begin shutting down, so mod should release system resources, close streams, etc.
+     */
+    @Override
+    public void stop() {
+        logger.logInfo("Realistic Stone is stopping.");
+    }
+}
